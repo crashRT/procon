b5810b91036bf7ce9fd6e3675052a8094a7575c7
crashRT <crashrt.farbauti@gmail.com>
2023-07-01 22:15:52 +0900

    Cわからん

--- ABC308/C - Standings.py
+++ ABC308/C - Standings.py
@@ -1,18 +1,15 @@
-import bisect
 import sys
 def I(): return int(sys.stdin.readline().rstrip())
 def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
 N = I()
 AB = [LI() for _ in range(N)]
 
-def calc_rate(a, b):
-    return b/(a+b)
+def calc_rate(data):
+    return data[0]/(data[0]+data[1])
 
-calc_rate_list = []
-for i in range(N):
-    bisect.insort_right(calc_rate_list, (i, calc_rate(AB[i][0], AB[i][1])), key=lambda x: x[1])
-
-print(calc_rate_list)
+calc_rate_list = [(i, calc_rate(data)) for i, data in enumerate(AB)]
+calc_rate_list.sort(key=lambda x: x[1], reverse=True)
 
 for man in calc_rate_list:
     print(man[0]+1, end=" ")
+print()
