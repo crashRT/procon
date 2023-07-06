c361d92678d3677b7ba517a14e6965045e0b37b8
crashRT <crashrt.farbauti@gmail.com>
2023-07-06 23:46:17 +0900

    YokanParty

--- practice/typical90/001_YokanParty.cpp
+++ practice/typical90/001_YokanParty.cpp
@@ -1,26 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int calc_min(int L, int N, int K, vector<int> A)
+bool can_cut_m(int L, int N, int K, vector<int> &B, int m)
 {
-    if (K == 0)
+    // すべてm以上で切れるか？
+    int cnt = 0;
+    int sum = 0;
+    for (int i = 0; i < N + 1; i++)
     {
-        return L;
+        sum += B.at(i);
+        if (sum >= m)
+        {
+            cnt++;
+            sum = 0;
+        }
     }
-
-    // 1個目の切れ目を選んだ場合
-    int min_len_rest = calc_min(L - A[0], N - 1, K - 1, vector<int>(A.begin() + 1, A.end()));
-    int min_len1 = min(A.at(0), min_len_rest);
-
-    if (K == N)
+    if (cnt >= K + 1)
     {
-        return min_len1;
+        return true;
     }
+    else
+    {
+        return false;
+    }
+}
 
-    // 1個目の切れ目を選ばなかった場合
-    A.at(1) = A.at(0) + A.at(1);
-    int min_len2 = calc_min(L, N - 1, K, vector<int>(A.begin() + 1, A.end()));
-    return max(min_len1, min_len2);
+int binary_search(int L, int N, int K, vector<int> &B)
+{
+    int left = 0;
+    int right = L;
+    while (right - left > 1)
+    {
+        int mid = left + (right - left) / 2;
+        if (can_cut_m(L, N, K, B, mid))
+        {
+            left = mid;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+    return left;
 }
 
 int main()
@@ -33,13 +54,15 @@ int main()
     {
         cin >> A.at(i);
     }
-    vector<int> B(N + 1);
+    vector<int> B(N + 1); // 各切れ込み間の長さ
     B.at(0) = A.at(0);
     for (int i = 0; i < N - 1; i++)
     {
         B.at(i + 1) = A.at(i + 1) - A.at(i);
     }
     B.at(N) = L - A.at(N - 1);
-    int ans = calc_min(L, N, K, B);
+
+    int ans = binary_search(L, N, K, B);
+
     cout << ans << endl;
 }
